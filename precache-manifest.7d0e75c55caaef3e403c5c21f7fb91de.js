cb5c08d763af6ded360f1d08330d755fa987f4e2
Adil-Bub <adilahmed1090@gmail.com>
2020-07-08 19:55:17 +0530

    Updates

---
+++ precache-manifest.7d0e75c55caaef3e403c5c21f7fb91de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd1b85c182954da3bb0b388ec4b3d829",
+    "url": "/thatmovie/index.html"
+  },
+  {
+    "revision": "5a1727b9510e381e92a3",
+    "url": "/thatmovie/static/css/main.dc41ed92.chunk.css"
+  },
+  {
+    "revision": "d31d47a5da9562a2cdb9",
+    "url": "/thatmovie/static/js/2.67f91546.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/thatmovie/static/js/2.67f91546.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a1727b9510e381e92a3",
+    "url": "/thatmovie/static/js/main.8607ad17.chunk.js"
+  },
+  {
+    "revision": "351a097551d5f6e9e351",
+    "url": "/thatmovie/static/js/runtime-main.03caf5a5.js"
+  }
+]);
